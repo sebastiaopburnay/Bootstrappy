ef9881eb9d65da43cdb24070f109c76005cbc5da
Sebastiao Burnay <sebastiao.burnay@thought-creator.com>
2018-04-05 09:41:07 +0100

    coisas boas

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//import './style.sass';
+
+const App = () => (
+    <div>
+        <h1>Hello, Zé dos bois!</h1>
+        <img src={"http://www.iotnet.mx/wp-content/uploads/2017/02/LogoTcreator_final-300x129.png"}/>
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
